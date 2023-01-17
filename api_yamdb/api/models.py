0a42b0d381e5a6ea515452af4cb9c83287ab60ad
Илья Афанасьев <iafanasevmail@gmail.com>
2023-01-17 23:33:14 +0300

    add_model

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -12,25 +12,3 @@ class Genres(models.Model):
         blank=True,
         max_length=50,
     )
-
-class Review(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-    )
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-    )
-    text = models.TextField()
-    pub_date = models.DateTimeField(
-        'Дата публикации',
-        auto_now_add=True,
-        db_index=True,
-    )
-
-    def __str__(self) -> str:
-        return self.text
-
